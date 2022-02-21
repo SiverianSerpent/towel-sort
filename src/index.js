42cf4b673ba39f823a5f35e3e8e9a487cab99132
Denis Kopych <denis.kopich@gmail.com>
2022-02-21 15:43:45 +0200

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (ma) {
+  if (!ma || !ma.length) { return []}
+  for (let key in ma) {
+    if (key % 2 !== 0) {
+     ma[key].sort((a,b) => (b - a))
+    }
+  }
+   let sigma = ma.join(',')
+   let bitu = sigma.split(',')
+   for (let key in bitu) {
+    bitu[key] = Number(bitu[key])
+   }
+   return bitu
 }
